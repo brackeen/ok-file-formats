fa17e1546a532776efba018f8c5a7360cd94eff6
David Brackeen <brackeen@gmail.com>
2016-12-31 12:19:27 -0800

    ok_wav: Test stereo sounds with different sound per channel

--- test/wav/wav_convert.sh
+++ test/wav/wav_convert.sh
@@ -7,7 +7,8 @@
 # brew install sox --with-libsndfile
 #
 
-IN=wav
+# Mono sound with even number of frames, Stereo sound with odd number of frames
+IN_WAVS=( wav/sound.wav wav/sound2.wav )
 OUT=gen
 
 mkdir -p $OUT
@@ -27,9 +28,10 @@ if command -v afconvert >/dev/null 2>&1; then
 
     for channels in `seq 1 2`;
     do
+        IN_WAV=${IN_WAVS[$channels - 1]}
         for data_format in "${caf_data_formats[@]}"
         do
-            afconvert -f caff -c $channels -d $data_format $IN/sound.wav $OUT/sound-${data_format}-${channels}ch.caf
+            afconvert -f caff -c $channels -d $data_format $IN_WAV $OUT/sound-${data_format}-${channels}ch.caf
         done
     
         # CAF-only raw files
@@ -46,11 +48,13 @@ fi
 echo ${0##*/}: Generating WAV sounds...
 for channels in `seq 1 2`;
 do
+    IN_WAV=${IN_WAVS[$channels - 1]}
+    
     # 8-bit WAVE files
     # Note that ulaw/alaw encoder for WAVE has slightly different output than the one for CAF, so it needs a seperate RAW file.
-    sox $IN/sound.wav --encoding unsigned-integer --bits 8 --channels $channels $OUT/sound-UI8-${channels}ch.wav
-    sox $IN/sound.wav --encoding mu-law --bits 8 --channels $channels $OUT/sound-ulaw-wav-${channels}ch.wav
-    sox $IN/sound.wav --encoding a-law --bits 8 --channels $channels $OUT/sound-alaw-wav-${channels}ch.wav
+    sox $IN_WAV --encoding unsigned-integer --bits 8 --channels $channels $OUT/sound-UI8-${channels}ch.wav
+    sox $IN_WAV --encoding mu-law --bits 8 --channels $channels $OUT/sound-ulaw-wav-${channels}ch.wav
+    sox $IN_WAV --encoding a-law --bits 8 --channels $channels $OUT/sound-alaw-wav-${channels}ch.wav
     
     # 8-bit RAW files
     sox $OUT/sound-UI8-${channels}ch.wav --channels $channels $OUT/sound-UI8-${channels}ch.raw
@@ -58,11 +62,11 @@ do
     sox $OUT/sound-alaw-wav-${channels}ch.wav --endian $system_endian --encoding signed-integer --bits 16 --channels $channels $OUT/sound-alaw-wav-${channels}ch.raw
     
     # Little endian WAVE files
-    sox $IN/sound.wav --endian little --encoding signed-integer --bits 16 --channels $channels $OUT/sound-LEI16-${channels}ch.wav
-    sox $IN/sound.wav --endian little --encoding signed-integer --bits 24 --channels $channels $OUT/sound-LEI24-${channels}ch.wav
-    sox $IN/sound.wav --endian little --encoding signed-integer --bits 32 --channels $channels $OUT/sound-LEI32-${channels}ch.wav
-    sox $IN/sound.wav --endian little --encoding floating-point --bits 32 --channels $channels $OUT/sound-LEF32-${channels}ch.wav
-    sox $IN/sound.wav --endian little --encoding floating-point --bits 64 --channels $channels $OUT/sound-LEF64-${channels}ch.wav
+    sox $IN_WAV --endian little --encoding signed-integer --bits 16 --channels $channels $OUT/sound-LEI16-${channels}ch.wav
+    sox $IN_WAV --endian little --encoding signed-integer --bits 24 --channels $channels $OUT/sound-LEI24-${channels}ch.wav
+    sox $IN_WAV --endian little --encoding signed-integer --bits 32 --channels $channels $OUT/sound-LEI32-${channels}ch.wav
+    sox $IN_WAV --endian little --encoding floating-point --bits 32 --channels $channels $OUT/sound-LEF32-${channels}ch.wav
+    sox $IN_WAV --endian little --encoding floating-point --bits 64 --channels $channels $OUT/sound-LEF64-${channels}ch.wav
     
     # Little endian RAW files
     sox $OUT/sound-LEI16-${channels}ch.wav --endian little $OUT/sound-LEI16-${channels}ch.raw
@@ -72,11 +76,11 @@ do
     sox $OUT/sound-LEF64-${channels}ch.wav --endian little $OUT/sound-LEF64-${channels}ch.raw  
 
     # Big endian WAVE files
-    sox $IN/sound.wav --endian big --encoding signed-integer --bits 16 --channels $channels $OUT/sound-BEI16-${channels}ch.wav
-    sox $IN/sound.wav --endian big --encoding signed-integer --bits 24 --channels $channels $OUT/sound-BEI24-${channels}ch.wav
-    sox $IN/sound.wav --endian big --encoding signed-integer --bits 32 --channels $channels $OUT/sound-BEI32-${channels}ch.wav
-    sox $IN/sound.wav --endian big --encoding floating-point --bits 32 --channels $channels $OUT/sound-BEF32-${channels}ch.wav
-    sox $IN/sound.wav --endian big --encoding floating-point --bits 64 --channels $channels $OUT/sound-BEF64-${channels}ch.wav
+    sox $IN_WAV --endian big --encoding signed-integer --bits 16 --channels $channels $OUT/sound-BEI16-${channels}ch.wav
+    sox $IN_WAV --endian big --encoding signed-integer --bits 24 --channels $channels $OUT/sound-BEI24-${channels}ch.wav
+    sox $IN_WAV --endian big --encoding signed-integer --bits 32 --channels $channels $OUT/sound-BEI32-${channels}ch.wav
+    sox $IN_WAV --endian big --encoding floating-point --bits 32 --channels $channels $OUT/sound-BEF32-${channels}ch.wav
+    sox $IN_WAV --endian big --encoding floating-point --bits 64 --channels $channels $OUT/sound-BEF64-${channels}ch.wav
     
     # Big endian RAW files
     sox $OUT/sound-BEI16-${channels}ch.wav --endian big $OUT/sound-BEI16-${channels}ch.raw
