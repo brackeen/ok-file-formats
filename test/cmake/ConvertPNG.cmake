0c2a808152a7b45bfb382cca18e2f2ccbd3fd4fb
David Brackeen <brackeen@gmail.com>
2017-02-11 11:27:35 -0800

    Tests: use ImageMagick 7

--- test/cmake/ConvertPNG.cmake
+++ test/cmake/ConvertPNG.cmake
@@ -3,11 +3,11 @@
 # The "-intensity Average" setting prevents ImageMagick from color-converting grayscale files.
 # For invalid files, a zero-byte output file is created.
 execute_process(
-    COMMAND convert -define png:exclude-chunk=gAMA ${IN_FILE} ${CMAKE_CURRENT_LIST_DIR}/temp.png
+    COMMAND magick -define png:bit-depth=8 -define png:exclude-chunk=gAMA ${IN_FILE} ${CMAKE_CURRENT_LIST_DIR}/temp.png
     ERROR_QUIET
 )
 if (EXISTS ${CMAKE_CURRENT_LIST_DIR}/temp.png)
-    execute_process(COMMAND convert -depth 8 -intensity Average ${CMAKE_CURRENT_LIST_DIR}/temp.png ${OUT_FILE})
+    execute_process(COMMAND magick -intensity Average ${CMAKE_CURRENT_LIST_DIR}/temp.png ${OUT_FILE})
     file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/temp.png)
 else()
     file(WRITE ${OUT_FILE} "")
