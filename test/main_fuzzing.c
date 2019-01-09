905708d92608353c94d1496c909624bdb96995de
David Brackeen <brackeen@gmail.com>
2019-01-09 08:25:12 -0800

    Add fnt, csv, and mo readers to the fuzz test

--- test/main_fuzzing.c
+++ test/main_fuzzing.c
@@ -1,6 +1,9 @@
 #include "ok_jpg.h"
 #include "ok_png.h"
 #include "ok_wav.h"
+#include "ok_fnt.h"
+#include "ok_csv.h"
+#include "ok_mo.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,8 +11,9 @@
 #ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
 
 static void printHelp() {
-    fprintf(stderr, "Reads a png, jpg, or wav file from stdin. Useful for fuzzing.\n\n");
-    fprintf(stderr, "One of these arguments required: --png --jpg --wav\n");
+    fprintf(stderr, "Reads a png, jpg, wav, fnt, csv, or mo file from stdin. "
+            "Useful for fuzzing.\n\n");
+    fprintf(stderr, "One of these arguments required: --png --jpg --wav --fnt --csv --mo\n");
     fprintf(stderr, "Options for png and jpg: --format-bgra --format-pre --format-flip\n");
 }
 
@@ -17,6 +21,9 @@ int main(int argc, char *argv[]) {
     int test_png = 0;
     int test_jpg = 0;
     int test_wav = 0;
+    int test_fnt = 0;
+    int test_csv = 0;
+    int test_mo = 0;
     ok_png_decode_flags png_flags = OK_PNG_COLOR_FORMAT_RGBA;
     ok_jpg_decode_flags jpg_flags = OK_JPG_COLOR_FORMAT_RGBA;
 
@@ -35,6 +42,12 @@ int main(int argc, char *argv[]) {
             test_jpg = 1;
         } else if (strcmp("--wav", argv[i]) == 0 || strcmp("--caf", argv[i]) == 0) {
             test_wav = 1;
+        } else if (strcmp("--fnt", argv[i]) == 0) {
+            test_fnt = 1;
+        } else if (strcmp("--csv", argv[i]) == 0) {
+            test_csv = 1;
+        } else if (strcmp("--mo", argv[i]) == 0) {
+            test_mo = 1;
         } else {
             fprintf(stderr, "Unrecognized argument: %s\n\n", argv[i]);
             printHelp();
@@ -43,7 +56,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if (test_png + test_jpg + test_wav != 1) {
+    if (test_png + test_jpg + test_wav + test_fnt + test_csv + test_mo != 1) {
         printHelp();
         abort(); // Prevent fuzzer from continuing
         return 1;
@@ -67,6 +80,24 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "%s\n", wav->error_message);
         }
         ok_wav_free(wav);
+    } else if (test_fnt) {
+        ok_fnt *fnt = ok_fnt_read(stdin);
+        if (fnt->error_message) {
+            fprintf(stderr, "%s\n", fnt->error_message);
+        }
+        ok_fnt_free(fnt);
+    } else if (test_csv) {
+        ok_csv *csv = ok_csv_read(stdin);
+        if (csv->error_message) {
+            fprintf(stderr, "%s\n", csv->error_message);
+        }
+        ok_csv_free(csv);
+    } else if (test_mo) {
+        ok_mo *mo = ok_mo_read(stdin);
+        if (mo->error_message) {
+            fprintf(stderr, "%s\n", mo->error_message);
+        }
+        ok_mo_free(mo);
     }
 
     return 0;
